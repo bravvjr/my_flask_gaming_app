94ed93b29e475d4f5b9b46a20b0d2b8019a2dcfb
Bravin Murambi <bravvjr@gmail.com>
2025-02-03 00:03:17 +0300

    postgreSQL migration test

--- website/__init__.py
+++ website/__init__.py
@@ -1,47 +1,53 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Initialize SQLAlchemy
 db = SQLAlchemy()
-DB_NAME = 'database.sqlite3'
-
-
-def create_database():
-    db.create_all()
-    print('Database Created')
-
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
+    # Set configuration from environment variables
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize SQLAlchemy with the app
     db.init_app(app)
 
+    # Error handler for 404
     @app.errorhandler(404)
     def page_not_found(error):
         return render_template('404.html')
 
+    # Initialize Flask-Login
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
+    # User loader for Flask-Login
     @login_manager.user_loader
     def load_user(id):
         return Customer.query.get(int(id))
 
+    # Import and register blueprints
     from .views import views
     from .auth import auth
     from .admin import admin
     from .models import Customer, Cart, Product, Order
 
-    app.register_blueprint(views, url_prefix='/') # localhost:5000/about-us
-    app.register_blueprint(auth, url_prefix='/') # localhost:5000/auth/change-password
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(admin, url_prefix='/')
 
-    # with app.app_context():
-    #     create_database()
+    # Create database tables (if they don't exist)
+    with app.app_context():
+        db.create_all()
 
     return app
-
